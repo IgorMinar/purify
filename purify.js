8b0b8c679d864ca823cd78edd4cd7454dd6db024
Kara Erickson <karakara@google.com>
2017-05-23 13:49:15 -0700

    feat: prefix InjectionToken, OverlayState, ConnectionPositionPair

--- purify.js
+++ purify.js
@@ -1,23 +1,18 @@
 module.exports = function(fileBody, fileName) {
     return fileBody
-      /* prefix downleveled classes w/ the @__PURE__ annotation */
-      .replace(
-          /^(var (\S+) = )(\(function \(\) \{\n(?:    (?:\/\*\*| \*|\*\/|\/\/)[^\n]*\n)*    function \2\([^\)]*\) \{\n)/mg,
-          '$1/*@__PURE__*/$3'
-      )
-      /* prefix downleveled classes that extend another class w/ the @__PURE__ annotation */
-      .replace(
-          /^(var (\S+) = )(\(function \(_super\) \{\n    __extends\(\2, _super\);\n)/mg,
-          '$1/*@__PURE__*/$3'
-      )
-        /* prefix new Optional() w/ the @__PURE__ annotation */
+        /* prefix downleveled classes w/ the @__PURE__ annotation */
         .replace(
-            /new __WEBPACK_IMPORTED_MODULE_\w+__angular_core__\["\w+" \/\* Optional \*\/]/mg,
-            '/*@__PURE__*/$&'
+            /^(var (\S+) = )(\(function \(\) \{\n(?:    (?:\/\*\*| \*|\*\/|\/\/)[^\n]*\n)*    function \2\([^\)]*\) \{\n)/mg,
+            '$1/*@__PURE__*/$3'
         )
-        /* prefix new SkipSelf() w/ the @__PURE__ annotation */
+        /* prefix downleveled classes that extend another class w/ the @__PURE__ annotation */
         .replace(
-            /new __WEBPACK_IMPORTED_MODULE_\w+__angular_core__\["\w+" \/\* SkipSelf \*\/]/mg,
+            /^(var (\S+) = )(\(function \(_super\) \{\n    __extends\(\2, _super\);\n)/mg,
+            '$1/*@__PURE__*/$3'
+        )
+        /* prefix new Optional/SkipSelf/InjectionToken/Inject w/ the @__PURE__ annotation */
+        .replace(
+            /new __WEBPACK_IMPORTED_MODULE_\w+__angular_core__\["\w+" \/\* (Optional|SkipSelf|InjectionToken|Inject) \*\/]/mg,
             '/*@__PURE__*/$&'
         )
         /* prefix all forwardRef callsites w/ the @__PURE__ annotation */
@@ -28,6 +23,11 @@ module.exports = function(fileBody, fileName) {
         /* prefix all mixinDisabled callsites w/ the @__PURE__ annotation */
         .replace(
             /mixinDisabled\(Md\S+\)/mg,
+            '/*@__PURE__*/$&'
+        )
+        /* prefix new OverlayState/ConnectionPositionPair w/ the @__PURE__ annotation */
+        .replace(
+            /new (OverlayState|ConnectionPositionPair)\(/mg,
             '/*@__PURE__*/$&'
         )
         /* prefix all animation trigger() callsites w/ the @__PURE__ annotation */
