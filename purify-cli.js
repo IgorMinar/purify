c26095e89e43e9fab8b4d7052506fe278a1ef2f5
Igor Minar <igor@angularjs.org>
2017-06-02 17:34:25 -0700

    fix: cli should pass filename through into the purify fn

--- purify-cli.js
+++ purify-cli.js
@@ -4,4 +4,4 @@ const pureReplace = require('./purify');
 const inputFile = process.argv[2];
 const inputFileBody = fs.readFileSync(inputFile).toString();
 
-console.log(pureReplace(inputFileBody), inputFile);
+console.log(pureReplace(inputFileBody, inputFile));
